be93af550a811b7e647efc963dc83a9fe4b5d0f3
Michael Leung <me@michaelleung.us>
2011-07-24 11:33:36 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+gem 'rails', '3.0.9'
+gem "bson_ext", "1.3.1"
+gem "mongoid", "2.0.2"
+gem "devise", "1.3.4"
+gem "rails-footnotes", ">= 3.7", :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moby::Application.config.secret_token = '5db8574b54f0e7d31a8b7a5cbea24d6b783d8cd51c114583d4a91ff6ab03c815996c466096d2213cc104234c9642e93ff1e89389bdf9ac6b9df89bd7208ab61c'
